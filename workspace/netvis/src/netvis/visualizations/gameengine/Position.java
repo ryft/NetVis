ec0d463beb0f3566ec4d0a6b1e27d0b283de1b93
Albert 'Errendir' Siddhartha Slawinski <errendir@gmail.com>
2013-04-19 18:28:51 +0100

    Restructurization part II

---
+++ workspace/netvis/src/netvis/visualizations/gameengine/Position.java
@@ -0,0 +1,17 @@
+package netvis.visualizations.gameengine;
+
+public class Position
+{
+	public Position(int posx, int posy) {
+		x = posx;
+		y = posy;
+	}
+	
+	public Position(double posx, double posy) {
+		x = (int) Math.round(posx);
+		y = (int) Math.round(posy);
+	}
+	
+	public int x;
+	public int y;
+}
